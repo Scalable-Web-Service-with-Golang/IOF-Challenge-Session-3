67c7d220a1990d36906b0dcdd44fb24c74d97a61
muhdjau <mhmd.jauhari@gmail.com>
2023-03-13 15:27:13 +0700

    Small-fix Commit

--- calcword.go
+++ calcword.go
@@ -3,14 +3,17 @@ package main
 import "fmt"
 
 func main() {
-	input := "selamat malam"
+	strInput := "selamat malam"
 	strCharCount := map[string]int{}
 
-	for i := 0; i < len(input); i++ {
-		str := string(input[i])
+	for i := 0; i < len(strInput); i++ {
+		str := string(strInput[i])
 
 		fmt.Println(str)
-		strCharCount[str]++
+	}
+
+	for _, val := range strInput {
+		strCharCount[string(val)]++
 	}
 
 	fmt.Println(strCharCount)
